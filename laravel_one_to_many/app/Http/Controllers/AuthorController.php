aaf22980d55c2a441ab280d5a31525a29a47142c
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-11-20 06:58:04 +0600

    author created

--- laravel_one_to_many/app/Http/Controllers/AuthorController.php
+++ laravel_one_to_many/app/Http/Controllers/AuthorController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    //
+    public function add_author(){
+        $author = new Author();
+        $author->username = 'rahim';
+        $author->password = 'rahim12';
+
+        $author->save();
+    }
 }
