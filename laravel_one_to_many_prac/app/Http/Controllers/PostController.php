c0f49079449df904c827361e5d5fcdec4f6d5e1e
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-11-26 14:14:49 +0600

    post submission okay

--- laravel_one_to_many_prac/app/Http/Controllers/PostController.php
+++ laravel_one_to_many_prac/app/Http/Controllers/PostController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function add_post($id){
         $author = Author::find($id);
-        var_dump($author);
+//        var_dump($author);
+        $post = new Post();
+        $post->title = 'Title 1';
+        $author->post()->save($post);
+
     }
 }
