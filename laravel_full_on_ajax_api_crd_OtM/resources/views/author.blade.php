a80f3e2cb557dfb2a3249eb807033413f60f42b9
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-12-20 08:42:04 +0600

    working on post page and bootstrap select column added

--- laravel_full_on_ajax_api_crd_OtM/resources/views/author.blade.php
+++ laravel_full_on_ajax_api_crd_OtM/resources/views/author.blade.php
@@ -37,7 +37,6 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-{{--                        <button type="button" class="btn btn-primary" >Save</button>--}}
                         <input type="submit" name="authorSave" id="authorSave" class="btn btn-primary" value="Save">
                     </div>
                     </form>
--- laravel_full_on_ajax_api_crd_OtM/resources/views/post.blade.php
+++ laravel_full_on_ajax_api_crd_OtM/resources/views/post.blade.php
@@ -1,7 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <h1 class="text-success text-center mt-5">Create Post</h1>
 
-    <h1 class="text-success text-center" style="margin-top: 200px;">Post</h1>
+        <!-- Button trigger modal -->
+        <div class="d-flex align-items-end flex-column">
+            <button type="button" class="btn btn-primary mt-5" data-bs-toggle="modal" data-bs-target="#authorModal">
+                Create Author
+            </button>
+        </div>
 
+        <!-- Modal -->
+        <div class="modal fade" id="authorModal" tabindex="-1" aria-labelledby="authorModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Create Author</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div  id="authorSuccessMessage"></div>
+                    <div class="modal-body">
+                        <form method="post" id="authorForm"  enctype="multipart/form-data">
+                            @csrf
+                            <div class="mb-3">
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="authorName" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="authorName" name="authorName" placeholder="Author Name" value="">
+                            </div>
+                            <div  id="authorName_error"></div>
+                            <div class="mb-3">
+                                <label for="authorPhoto" class="form-label">Photo</label>
+                                <input type="file" class="form-control" name="authorPhoto" id="authorPhoto" >
+                            </div>
+                            <div id="authorPhoto_error"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <input type="submit" name="authorSave" id="authorSave" class="btn btn-primary" value="Save">
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
