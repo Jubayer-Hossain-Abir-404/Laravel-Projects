c1b7aadddf7f86be0e9474243308e22e7df13b84
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-12-28 23:21:11 +0600

    category page view done

--- laravel_full_on_ajax_api_crd_OtM/resources/views/layouts/app.blade.php
+++ laravel_full_on_ajax_api_crd_OtM/resources/views/layouts/app.blade.php
@@ -25,6 +25,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="{{ route('post') }}">Post</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('category') }}">Category</a>
+                    </li>
                 </ul>
                 <div class="d-flex">
                     Log
--- laravel_full_on_ajax_api_crd_OtM/routes/web.php
+++ laravel_full_on_ajax_api_crd_OtM/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('author', [AuthorController::class, 'index'])->name('author');
 
 Route::get('post', [PostController::class, 'index'])->name('post');
+
+Route::get('category', [CategoryController::class, 'index'])->name('category');
