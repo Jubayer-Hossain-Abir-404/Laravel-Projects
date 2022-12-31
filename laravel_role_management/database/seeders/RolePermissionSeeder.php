3ee78e752c7fce1c8c9ff569652b75026a1f11a1
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-12-31 23:03:01 +0600

    role and permission set using seeder

--- laravel_role_management/database/seeders/RolePermissionSeeder.php
+++ laravel_role_management/database/seeders/RolePermissionSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -13,6 +15,51 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // create Roles
+        $roleSuperAdmin = Role::create(['name' => 'superAdmin']);
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleEditor = Role::create(['name' => 'editor']);
+        $roleUser = Role::create(['name' => 'user']);
+
+//        permission list as array
+        $permissions = [
+
+            // Dashboard
+            'dashboard.view',
+
+
+            //Blog Permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            //Admin Permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            //Role Permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            //Profile Permissions
+            'profile.view',
+            'profile.edit',
+        ];
+//        Create and Assign Permissions
+//        $permission = Permission::create(['name' => 'edit articles']);
+        for($i=0;$i<count($permissions);$i++){
+//            create permission
+            $permission = Permission::create(['name' => $permissions[$i]]);
+            $roleSuperAdmin->givePermissionTo($permission);
+            $permission->assignRole($roleSuperAdmin);
+        }
     }
 }
