e53f3771b8eea91df1760cdd61a8001fd2649b72
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-01-18 08:37:33 +0600

    working on edit page

--- laravel_role_management/resources/views/backend/pages/users/edit.blade.php
+++ laravel_role_management/resources/views/backend/pages/users/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('backend.layouts.master')
 
 @section('title')
-    Role Edit ~ Admin Panel
+    User Edit ~ Admin Panel
 @endsection
 
 @section('styles')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <style>
         .form-check-label {
             text-transform: capitalize;
@@ -16,21 +17,21 @@
     <div>
         <!-- page title area start -->
         <div class="page-title-area">
-            <div class="row align-items-center">
+            <User class="row align-items-center">
                 <div class="col-sm-6">
                     <div class="breadcrumbs-area clearfix">
-                        <h4 class="page-title pull-left">Role Edit - {{$role->name}}</h4>
+                        <h4 class="page-title pull-left">User Create</h4>
                         <ul class="breadcrumbs pull-left">
                             <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                            <li><a href="{{ route('admin.roles.index') }}">All Roles</a></li>
-                            <li><span>Edit Role</span></li>
+                            <li><a href="{{ route('admin.users.index') }}">All Users</a></li>
+                            <li><span>Create User</span></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-6 clearfix">
                     @include('backend.layouts.partials.logout')
                 </div>
-            </div>
+            </User
         </div>
         <!-- page title area end -->
         <div class="main-content-inner">
@@ -39,57 +40,44 @@
                 <div class="col-12 mt-5">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="header-title">Edit Role</h4>
+                            <h4 class="header-title">Create New Role</h4>
                             @include('backend.layouts.partials.messages')
 
-                            <form action="{{route('admin.roles.update', $role->id)}}" method="POST">
-                                @method('PUT')
+                            <form action="{{route('admin.users.store')}}" method="POST">
                                 @csrf
-                                <div class="form-group">
-                                    <label for="name">Role Name</label>
-                                    <input type="text" name="name" class="form-control" value="{{$role->name}}" id="name"  placeholder="Enter a Role Name">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="name">Permissions</label>
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input"  id="checkPermissionAll" value="1" {{\App\Models\User::roleHasPermissions($role, $all_permissions) ? 'checked' : ''}}>
-                                        <label class="form-check-label" for="checkPermissionAll">All</label>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6 col-sm-12">
+                                        <label for="name">User Name</label>
+                                        <input type="text" name="name" class="form-control" id="name"  placeholder="Enter a Name">
                                     </div>
-                                </div>
-                                <hr>
-                                @php $i=1; @endphp
-                                @foreach($permission_groups as $group)
-                                    <div class="row">
-                                        @php
-                                            $permissions = \App\Models\User::getPermissionByGroupName($group->name);
-                                            $j=1;
-                                        @endphp
-                                        <div class="col-3">
-                                            <div class="form-check">
-                                                <input type="checkbox" class="form-check-input"  id="{{ $i }}Management" value="{{ $group->name }}" onclick="checkPermissionByGroup('role-{{$i}}-management-checkbox', this)" {{\App\Models\User::roleHasPermissions($role, $permissions) ? 'checked' : ''}}>
-                                                <label class="form-check-label" for="checkPermission">{{$group->name}}</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-9 role-{{$i}}-management-checkbox">
-                                            <div class="form-group">
-                                                @foreach($permissions as $permission)
-                                                    <div class="form-check">
-                                                        <input type="checkbox" class="form-check-input" onclick="checkSinglePermission('role-{{$i}}-management-checkbox', '{{ $i }}Management', {{count($permissions)}})" name="permissions[]" {{$role->hasPermissionTo($permission->name) ? 'checked' : '' }} id="checkPermission{{ $permission->id }}" value="{{ $permission->name }}">
-                                                        <label class="form-check-label" for="checkPermission{{ $permission->id }}">{{$permission->name}}</label>
-                                                    </div>
-                                                    @php
-                                                        $j++;
-                                                    @endphp
-                                                @endforeach
-                                                <br>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @php $i++; @endphp
-                                @endforeach
 
-                                <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4">Update Role</button>
+                                    <div class="form-group col-md-6 col-sm-12">
+                                        <label for="email">User Email</label>
+                                        <input type="text" name="email" class="form-control" id="email"  placeholder="Enter Email">
+                                    </div>
+
+                                    <div class="form-group col-md-6 col-sm-12">
+                                        <label for="password">Password</label>
+                                        <input type="password" name="password" class="form-control" id="password"  placeholder="Password">
+                                    </div>
+
+                                    <div class="form-group col-md-6 col-sm-12">
+                                        <label for="password_confirmation">Confirm Password</label>
+                                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation"  placeholder="Confirm Password">
+                                    </div>
+
+                                    <div class="form-group col-md-6 col-sm-12">
+                                        <label for="roles">Assign Roles</label>
+                                        <select name="roles[]" id="roles" class="select2" multiple>
+                                            @foreach ($roles as $role)
+                                                <option value="{{ $role->name }}">{{ $role->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                </div>
+                                
+                                <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4">Save User</button>
                             </form>
                         </div>
                     </div>
@@ -102,5 +90,12 @@
 @endsection
 
 @section('scripts')
-    @include('backend.pages.roles.partials.scripts')
+
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.select2').select2();
+        });
+    </script>
 @endsection
