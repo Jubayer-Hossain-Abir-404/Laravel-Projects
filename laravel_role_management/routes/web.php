ee09f6e04990d8f04f3f001a1ec2842a4627fb71
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-01-22 00:15:57 +0600

    path set and auth directory restructure

--- laravel_role_management/routes/web.php
+++ laravel_role_management/routes/web.php
@@ -28,4 +28,15 @@ Route::group(['prefix' => 'admin'], function () {
    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
    Route::resource('roles', RolesController::class, ['names' => 'admin.roles']);
    Route::resource('users', UsersController::class, ['names' => 'admin.users']);
+
+   // Login Routes
+   Route::get('/login',[LoginController::class, 'showLoginForm'])->name('admin.login');
+   Route::post('/login/submit',[LoginController::class, 'login'])->name('admin.login.submit');
+
+   // Logout Routes
+   Route::post('/logout/submit',[LoginController::class, 'logout'])->name('admin.logout.submit');
+
+   // Forget Password Routes
+   Route::get('/password/reset',[ForgotPasswordController::class, 'showLinkRequestForm'])->name('admin.password.request');
+   Route::post('/password/reset/submit',[ForgotPasswordController::class, 'reset'])->name('admin.password.update');
 });
