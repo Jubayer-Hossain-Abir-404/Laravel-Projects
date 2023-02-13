9bf725761447eb147db96226f8d34412590c2479
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-02-13 08:06:13 +0600

    sidebar wise role right set

--- laravel_role_management/resources/views/backend/layouts/partials/sidebar.blade.php
+++ laravel_role_management/resources/views/backend/layouts/partials/sidebar.blade.php
@@ -12,21 +12,29 @@
         <div class="menu-inner">
             <nav>
                 <ul class="metismenu" id="menu">
-                    <li class="active">
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
-                        <ul class="collapse">
-                            <li class="{{\Illuminate\Support\Facades\Route::is('admin.dashboard') ? 'active' : ''}}"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-tasks"></i><span>
-                                        Roles & Permissions
-                                    </span></a>
-                        <ul class="collapse {{\Illuminate\Support\Facades\Route::is('admin.roles.create') || \Illuminate\Support\Facades\Route::is('admin.roles.index') || \Illuminate\Support\Facades\Route::is('admin.roles.edit') || \Illuminate\Support\Facades\Route::is('admin.roles.show') ? 'in' : ''}}">
-                            <li class="{{\Illuminate\Support\Facades\Route::is('admin.roles.index') || \Illuminate\Support\Facades\Route::is('admin.roles.edit') || \Illuminate\Support\Facades\Route::is('admin.roles.show') ? 'active' : ''}}"><a href="{{route('admin.roles.index')}}">All Roles</a></li>
-                            <li class="{{\Illuminate\Support\Facades\Route::is('admin.roles.create') ? 'active' : ''}}"><a href="{{route('admin.roles.create')}}">Create Role</a></li>
-                        </ul>
-                    </li>
+                    @if ($usr->can('dashboard.view'))
+                        <li class="active">
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                            <ul class="collapse">
+                                <li class="{{\Illuminate\Support\Facades\Route::is('admin.dashboard') ? 'active' : ''}}"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                    @if ($usr->can('role.view') || $usr->can('role.create') || $usr->can('role.edit') ||  $usr->can('role.delete'))
+                        <li>
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-tasks"></i><span>
+                                            Roles & Permissions
+                                        </span></a>
+                            <ul class="collapse {{\Illuminate\Support\Facades\Route::is('admin.roles.create') || \Illuminate\Support\Facades\Route::is('admin.roles.index') || \Illuminate\Support\Facades\Route::is('admin.roles.edit') || \Illuminate\Support\Facades\Route::is('admin.roles.show') ? 'in' : ''}}">
+                                @if ($usr->can('role.view'))
+                                    <li class="{{\Illuminate\Support\Facades\Route::is('admin.roles.index') || \Illuminate\Support\Facades\Route::is('admin.roles.edit') || \Illuminate\Support\Facades\Route::is('admin.roles.show') ? 'active' : ''}}"><a href="{{route('admin.roles.index')}}">All Roles</a></li>
+                                @endif
+                                @if ($usr->can('role.create'))
+                                    <li class="{{\Illuminate\Support\Facades\Route::is('admin.roles.create') ? 'active' : ''}}"><a href="{{route('admin.roles.create')}}">Create Role</a></li>
+                                @endif
+                            </ul>
+                        </li>
+                    @endif
 
                     @if ($usr->can('admin.view') || $usr->can('admin.create') || $usr->can('admin.edit') ||  $usr->can('admin.delete'))
                         <li>
