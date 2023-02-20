bbcddde471c2e88eb1433085f643a3cd54ec16a3
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-02-20 08:33:30 +0600

    role mangement complete

--- laravel_role_management/resources/views/backend/pages/dashboard/index.blade.php
+++ laravel_role_management/resources/views/backend/pages/dashboard/index.blade.php
@@ -47,7 +47,7 @@
                                         <a href="{{ route('admin.admins.index') }}">
                                             <div class="p-4 d-flex justify-content-between align-items-center">
                                                 <div class="seofct-icon"><i class="fa fa-user"></i> Admins</div>
-                                                <h2>{{ $total_admins }}</h2>
+                                                <h2 class="text-white">{{ $total_admins }}</h2>
                                             </div>
                                         </a>
                                     </div>
