43f511ee51668e2d5987d0d67e0907c676be5565
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-02-26 22:34:56 +0600

    stripe form page showing

--- laravel_stripe_payment_method/routes/web.php
+++ laravel_stripe_payment_method/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/stripe', [StripeController::class,'stripePyament'])->name("stripe.post");
