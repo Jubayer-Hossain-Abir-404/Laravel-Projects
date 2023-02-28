6e612016b0ee1b34ae208a00c1ef5ff4781110d0
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-02-28 08:55:34 +0600

    stripe tutorial complete

--- laravel_stripe_payment_method/app/Http/Controllers/StripeController.php
+++ laravel_stripe_payment_method/app/Http/Controllers/StripeController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Stripe;
 use Session;
 
+
+
 use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
     public function stripePyament(Request $req)
     {
-    	//print_r($req->all()); die();
+    	// print_r($req->all()); die();
+
     	Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
     	$data = Stripe\Charge::create([
     			"amount"=>200*100,
@@ -19,7 +22,7 @@ class StripeController extends Controller
     			"source"=>$req->stripeToken,
     			"description"=>"Test payment from expert rohila 2"
     	]);
-        echo "<pre>"; print_r($data); die();
+        // echo "<pre>"; print_r($data); die();
 
     	Session::flash("success","Payment successfully!");
 
