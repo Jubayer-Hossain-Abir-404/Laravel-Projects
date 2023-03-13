3c08133cdf91add79f92e4441f4465985c80b997
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-03-13 08:33:45 +0600

    checks if session exists or not and display 0 taka if not

--- laravel_add_to_cart_checkout/app/Http/Controllers/IndexController.php
+++ laravel_add_to_cart_checkout/app/Http/Controllers/IndexController.php
@@ -8,11 +8,14 @@ class IndexController extends Controller
 {
     public function index(){
         $products = Product::latest('id')->get();
-        $send_cart = session()->get('cart');
-        $price=0;
-        foreach($send_cart as $cart){
-            $price += (float) $cart['price'] * (float) $cart['quantity']; 
+        $price = 0;
+        if(session()->has('cart')){
+            $send_cart = session()->get('cart');
+            foreach ($send_cart as $cart) {
+                $price += (float) $cart['price'] * (float) $cart['quantity'];
+            }
         }
+
         return view('index', compact('products', 'price'));
     }
 }
