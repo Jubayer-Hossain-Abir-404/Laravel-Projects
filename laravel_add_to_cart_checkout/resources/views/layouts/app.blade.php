f02cd894480ae41a794d59975c19c40f489e3287
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-03-22 08:21:35 +0600

    create timer page created with design and linkup and controller function

--- laravel_add_to_cart_checkout/resources/views/layouts/app.blade.php
+++ laravel_add_to_cart_checkout/resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap5.min.css" rel="stylesheet"></link>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    @yield('css')
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -26,6 +28,11 @@
                     @if (Auth::check())
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="{{ route('profile') }}">Profile</a>
+                        </li>
+                    @endif
+                    @if (Auth::check())
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('timer.create') }}">Create Timer</a>
                         </li>
                     @endif
                 </ul>
---
+++ laravel_add_to_cart_checkout/resources/views/timer/create.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('css')
+    <style>
+        a {
+            color: #BE206B !important;
+        }
+
+        a.btn.btn-lg.btn-block,
+        .btn-info {
+            color: white !important;
+        }
+
+        .btn-secondary {
+            background-color: #448BC6 !important;
+            color: #fff !important;
+        }
+
+        button.btn.btn.btn-secondary {
+            width: 100%;
+        }
+
+        h3 {
+            text-align: center;
+            line-height: 200%;
+        }
+
+        .collpa .pt-0,
+        .py-0 {
+            padding-top: 0 !important;
+        }
+    </style>
+@endsection
+@section('content')
+    <main>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="main">
+                        <h3><a>Countdown Timer Example in Laravel.</a></h3>
+                        <form role="form" action="{{ route('timer.update') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Select Date Time <span class="text-danger">*</span></label>
+                                <input type="datetime-local" name="date_time" class="form-control" />
+                            </div>
+                            <div class="form-group">
+                                <label for="author">Status<span class="text-danger">*</span></label>
+                                <select class="form-control" name="timer_status">
+                                    <option value="0">Inactive</option>
+                                    <option value="1">Active</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn btn-secondary">save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
