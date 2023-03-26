f335bf5dff3c919a1e96c3a7fdff7aed7b1d4447
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-03-26 15:52:02 +0600

    public and protected routes distinguished

--- laravel_sanctum_api/routes/api.php
+++ laravel_sanctum_api/routes/api.php
@@ -18,17 +18,24 @@ use App\Http\Controllers\ProductController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+// public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
+
+// protected routes
 Route::group(['middleware'=> ['auth:sanctum']], function () {
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
 
-// Route::get('/products', [ProductController::class, 'index']);
 
-// Route::post('/products', [ProductController::class, 'store']);
 
-Route::resource('products', ProductController::class);
+
+// Route::resource('products', ProductController::class);
 
 
 
