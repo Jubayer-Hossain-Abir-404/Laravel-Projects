99972adacd2c85242b1aeec2cf600cd9dce09227
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-04-02 21:21:04 +0600

    forget password project index, login, register layout ready

---
+++ laravel_forget_reset_password/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 class="text-success text-center" style="margin-top: 200px;">This is the Home Page</h1>
+
+@endsection
--- laravel_forget_reset_password/routes/web.php
+++ laravel_forget_reset_password/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('author', [AuthorController::class, 'index'])->name('author');
+
+    Route::get('post', [PostController::class, 'index'])->name('post');
+
+    Route::get('category', [CategoryController::class, 'index'])->name('category');
+
+    Route::get('/logout', [RegisterController::class, 'logout'])->name('logout');
+});
+
+Route::get('/', [IndexController::class, 'index'])->name('home');
+
+//this is for login
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [RegisterController::class, 'loginPage'])->name('login');
+
+    Route::get('/register', [RegisterController::class, 'registerPage'])->name('register');
+
+    Route::post('/register/submit', [RegisterController::class, 'submitRegister'])->name('submitRegister');
+
+    Route::post('/login/submit', [RegisterController::class, 'submitLogin'])->name('submitLogin');
 });
