1eb6f231ffe40a5d00fbeda9d2822e1b76109e70
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-04-24 09:14:30 +0600

    user page shown on the root page

--- e-Commerce-Pro-Tutorial/app/Http/Controllers/HomeController.php
+++ e-Commerce-Pro-Tutorial/app/Http/Controllers/HomeController.php
@@ -11,6 +11,10 @@ use App\Models\User;
 
 class HomeController extends Controller
 {
+
+    public function index(){
+        return view('home.userpage');
+    }
     public function redirect(){
         $usertype = Auth::user()->usertype;
 
