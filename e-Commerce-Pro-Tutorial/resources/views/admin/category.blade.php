c41c44c7afd41287841cb9b49651dea791635f02
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2023-04-27 07:46:00 +0600

    category form and model created

--- e-Commerce-Pro-Tutorial/resources/views/admin/category.blade.php
+++ e-Commerce-Pro-Tutorial/resources/views/admin/category.blade.php
@@ -6,13 +6,26 @@
             text-align: center;
             padding-top: 40px;
         }
+        .h2Font{
+            font-size: 40px;
+            padding-bottom: 40px;
+        }
+        .input_color{
+            color: black;
+        }
     </style>
 @endsection
 
 @section('admin-content')
 
     <div class="div_center">
-        <h2>Add Category</h2>
+        <h2 class="h2Font">Add Category</h2>
+
+        <form action="">
+            <input class="input_color" type="text" name="name" placeholder="Write category name">
+
+            <input type="submit" class="btn btn-primary" name="submit" value="Add Category">
+        </form>
     </div>
 
 
